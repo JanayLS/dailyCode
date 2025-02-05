61fdcd990bcd48fa01dd9b96a554cdf185b6fc75
Janay S <145382048+JanayLS@users.noreply.github.com>
2025-02-05 08:52:05 -0500

    Update DoIt.java

--- Week_1/Day_8/DoIt.java
+++ Week_1/Day_8/DoIt.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-class DoIt {
+class Main {
     public static void main(String[] args) {
     // BEGIN
     // Declare a variable for user input
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a positive number: ");
-    //     Read input
-    int userInteger = scanner.nextInt();
     // REPEAT AT LEAST ONCE
     // DO
+    int userInteger;
     do {
-        System.out.println("Invalid input. Try again.");
         System.out.print("Enter a positive number: ");
         //     Read input
         userInteger = scanner.nextInt();
+        if (userInteger <= 0) {
+            System.out.println("Invalid input. Try again.");
+        }
     }
 
     // WHILE (input is negative)
     while(userInteger <= 0);
-    
-    System.out.println("You entered: " + userInteger);
+     System.out.println("You entered: " + userInteger);
+
     // END
     scanner.close();
 
